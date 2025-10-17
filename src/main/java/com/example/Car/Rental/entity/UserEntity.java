d712d81438bd58d150d8e542354e9bc9b8dd0322
abdulahad007-ui <AhadScars>
2025-10-17 17:25:33 +0530

    add Role in Entity

--- src/main/java/com/example/Car/Rental/entity/UserEntity.java
+++ src/main/java/com/example/Car/Rental/entity/UserEntity.java
@@ -1,34 +1,31 @@
 package com.example.Car.Rental.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.Car.Rental.role.Role;
+import jakarta.persistence.*;
 
 @Entity
 public class UserEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    private String username;
-    private String password;
-
-    public UserEntity(Integer id, String name, String password) {
+    public UserEntity(Integer id, String username, String password, Role role) {
         this.id = id;
-        this.username = name;
+        this.username = username;
         this.password = password;
+        this.role = role;
     }
+
     public UserEntity(){
 
     }
 
-    public Integer getId () {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId (Integer id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -40,11 +37,24 @@ public class UserEntity {
         this.username = username;
     }
 
-    public String getPassword () {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword (String password) {
+    public void setPassword(String password) {
         this.password = password;
     }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
